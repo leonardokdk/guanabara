089deaca87b51d1c119172acdccde0621347ddb6
leonardokdk <ssleonardo29@gmail.com>
2025-03-14 19:18:12 -0400

    Adiciona resolução do exercicio func/parte2/finc1

--- func/parte2/func1.py
+++ func/parte2/func1.py
@@ -1,3 +1,20 @@
-""" Crie um programa que tenha uma função chamada voto() que vai receber como parâmetro o ano de nascimento
-de uma pessoa, retornando um valor literal indicando se uma pessoa tem voto NEGADO, OPCIONAL e
-OBRIGATÓRIO nas eleições. """
+""" Crie um programa que tenha uma função chamada voto() que vai receber como parâmetro o ano de 
+nascimentode uma pessoa, retornando um valor literal indicando se uma pessoa tem voto NEGADO, 
+OPCIONAL e OBRIGATÓRIO nas eleições. """
+
+def voto(anoDeNascimento):
+    idade = 2025 - anoDeNascimento 
+    if idade >= 65:
+        voto = 'Voto Opcional'
+    elif idade >= 18 and idade < 65:
+        voto= 'Voto Obrigatorio'
+    elif idade >= 16:
+        voto = 'Voto opcional'
+    else:
+        voto = 'Não pode votar'
+    
+    return voto
+
+
+resultado = voto(2010)
+print(resultado)
